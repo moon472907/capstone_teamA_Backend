0e40fcf42f381ebf9c151eed0d8285168e25efb8
jaong <90236870+parkjioh@users.noreply.github.com>
2025-09-30 11:40:07 +0900

    프롬포터에 성별 나이 추가

--- backend/src/main/java/com/back/domain/mission/service/AiMissionGeneratorService.java
+++ backend/src/main/java/com/back/domain/mission/service/AiMissionGeneratorService.java
@@ -1,10 +1,15 @@
 package com.back.domain.mission.service;
 
+import com.back.domain.member.entity.Member;
+import com.back.domain.member.repository.MemberRepository;
 import com.back.domain.mission.dto.ai.AiMissionResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 
 @Service
@@ -69,14 +74,23 @@ public class AiMissionGeneratorService {
     }*/
 
     private final ChatClient chatClient;
+    private final MemberRepository memberRepository;
 
     public AiMissionResult generateMission(String rawGoal, int weeks, Integer memberId) {
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new RuntimeException("회원이 존재하지 않습니다."));
+
+        int age = Period.between(member.getBirth(), LocalDate.now()).getYears();
+        String gender = member.getGender().name(); // "MALE", "FEMALE", "NONE"
+
 
         String promptText = """
             너는 미션 플래너야.
             사용자가 목표와 기간을 입력하면 반드시 아래 JSON 구조를 채워서 출력해라.
             설명은 하지 말고 JSON만 반환해라.
             그리고 제목은 간결하게 정리해서 반환해줘.
+            그리고 나이나 성별을 고려할 수 있다면 고려해서 만들어줘.
 
             JSON 형식:
             {{
@@ -97,7 +111,9 @@ public class AiMissionGeneratorService {
             사용자 입력:
             목표: %s
             기간: %d주
-            """.formatted(rawGoal, weeks);
+            나이 : %d세
+            성별 : %s
+            """.formatted(rawGoal, weeks, age, gender);
 
         Prompt prompt = new Prompt(promptText);
 
