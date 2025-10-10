1a078f81e8a3f301534106c441a161303ff56195
안홍선 <moon72907@gmail.com>
2025-10-10 10:20:39 +0900

    DEBUG : 아이템 타입 도메인수정으로 발생한 테스트케이스 수정

--- backend/src/main/java/com/back/domain/title/dto/CreateTitleDto.java
+++ backend/src/main/java/com/back/domain/title/dto/CreateTitleDto.java
@@ -2,11 +2,13 @@ package com.back.domain.title.dto;
 
 public record CreateTitleDto(
         String content,
-        String achiveRequire
+        String achiveRequire,
+        String caption
 ) {
-    public CreateTitleDto(String content, String achiveRequire)
+    public CreateTitleDto(String content, String achiveRequire,      String caption)
     {
         this.content = content;
         this.achiveRequire = achiveRequire;
+        this.caption = caption;
     }
 }
